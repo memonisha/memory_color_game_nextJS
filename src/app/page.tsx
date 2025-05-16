96e9c13d07f36139883a7e6c89c032752b5e9aac
memonisha <bloggermonisha@gmail.com>
2025-05-16 08:21:53 +0530

    css edits

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,11 @@ export default function Home() {
     <>
       <Header />
       <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-        <h2 className="text-4xl font-bold mb-4">Welcome to the Memory Color Game 🎮</h2>
-        <p className="text-lg">Test your memory and have fun!</p>
+        <h2 className="text-4xl font-bold mb-4">💡Welcome to the Color Memory Game 🎮</h2>
+<p className="text-lg"> Focus, breathe, and don’t blink...Each level gets trickier... 🐣!</p>
+        <br></br>
+        <p> 🧠 Stay sharp, Simon’s watching👀  </p>
+
       </div>
     </>
   );
